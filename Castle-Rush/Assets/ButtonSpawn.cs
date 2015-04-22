e8272b8351410ee1bd8e4934ff8035d40d8dd05c
Scott Stephan <ss2111@msstate.edu>
2015-04-22 10:05:17 -0500

    Fixed the Buttons
    The buttons now function and cleaned up some code.

--- Castle-Rush/Assets/ButtonSpawn.cs
+++ Castle-Rush/Assets/ButtonSpawn.cs
@@ -32,7 +32,31 @@ public class ButtonSpawn : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			if (PhotonNetwork.player.ID == PhotonNetwork.masterClient.ID) {
+				GameUI.score = 5000;
+			}
+			else{
+				GameUI.score = 5000;
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.X)) {
+			Obj1();
+		}
+		if (Input.GetKeyDown (KeyCode.C)) {
+			Obj2();
+		}
+		if (Input.GetKeyDown (KeyCode.V)) {
+			Obj3();
+		}
+		if (Input.GetKeyDown (KeyCode.B)) {
+			Obj4();
+		}
+		if (Input.GetKeyDown (KeyCode.N)) {
+			Obj5();
+		}
+
 	}
 
 	public void Obj1(){
@@ -56,17 +80,17 @@ public class ButtonSpawn : MonoBehaviour {
 	
 	public void Obj2(){
 		if (PhotonNetwork.player.ID == PhotonNetwork.masterClient.ID) {
-			if(GameUI.score >= unit1Cost){
+			if(GameUI.score >= unit2Cost){
 				player1ObjectSpawnPosition.x = Random.Range(-8f,-14f);
 				PhotonNetwork.Instantiate (player1Obj2.name, player1ObjectSpawnPosition, Quaternion.identity, 0, null);
-				GameUI.score -= unit1Cost;
+				GameUI.score -= unit2Cost;
 			}
 		} 
 		else {
-			if(GameUI.score >= unit1Cost){
+			if(GameUI.score >= unit2Cost){
 				player2ObjectSpawnPosition.x = Random.Range(-8f,-14f);
 				PhotonNetwork.Instantiate (player2Obj2.name, player2ObjectSpawnPosition, Quaternion.identity, 0, null);
-				GameUI.score -= unit1Cost;
+				GameUI.score -= unit2Cost;
 			}
 		}
 	}
@@ -74,17 +98,17 @@ public class ButtonSpawn : MonoBehaviour {
 	
 	public void Obj3(){
 		if (PhotonNetwork.player.ID == PhotonNetwork.masterClient.ID) {
-			if(GameUI.score >= unit1Cost){
+			if(GameUI.score >= unit3Cost){
 				player1ObjectSpawnPosition.x = Random.Range(-8f,-14f);
 				PhotonNetwork.Instantiate (player1Obj3.name, player1ObjectSpawnPosition, Quaternion.identity, 0, null);
-				GameUI.score -= unit1Cost;
+				GameUI.score -= unit3Cost;
 			}
 		} 
 		else {
-			if(GameUI.score >= unit1Cost){
+			if(GameUI.score >= unit3Cost){
 				player2ObjectSpawnPosition.x = Random.Range(-8f,-14f);
 				PhotonNetwork.Instantiate (player2Obj3.name, player2ObjectSpawnPosition, Quaternion.identity, 0, null);
-				GameUI.score -= unit1Cost;
+				GameUI.score -= unit3Cost;
 			}
 		}
 	}
@@ -92,17 +116,17 @@ public class ButtonSpawn : MonoBehaviour {
 	
 	public void Obj4(){
 		if (PhotonNetwork.player.ID == PhotonNetwork.masterClient.ID) {
-			if(GameUI.score >= unit1Cost){
+			if(GameUI.score >= unit4Cost){
 				player1ObjectSpawnPosition.x = Random.Range(-8f,-14f);
 				PhotonNetwork.Instantiate (player1Obj4.name, player1ObjectSpawnPosition, Quaternion.identity, 0, null);
-				GameUI.score -= unit1Cost;
+				GameUI.score -= unit4Cost;
 			}
 		} 
 		else {
-			if(GameUI.score >= unit1Cost){
+			if(GameUI.score >= unit4Cost){
 				player2ObjectSpawnPosition.x = Random.Range(-8f,-14f);
 				PhotonNetwork.Instantiate (player2Obj4.name, player2ObjectSpawnPosition, Quaternion.identity, 0, null);
-				GameUI.score -= unit1Cost;
+				GameUI.score -= unit4Cost;
 			}
 		}
 	}
@@ -110,17 +134,17 @@ public class ButtonSpawn : MonoBehaviour {
 	
 	public void Obj5(){
 		if (PhotonNetwork.player.ID == PhotonNetwork.masterClient.ID) {
-			if(GameUI.score >= unit1Cost){
+			if(GameUI.score >= unit5Cost){
 				player1ObjectSpawnPosition.x = Random.Range(-8f,-14f);
 				PhotonNetwork.Instantiate (player1Obj5.name, player1ObjectSpawnPosition, Quaternion.identity, 0, null);
-				GameUI.score -= unit1Cost;
+				GameUI.score -= unit5Cost;
 			}
 		} 
 		else {
-			if(GameUI.score >= unit1Cost){
+			if(GameUI.score >= unit5Cost){
 				player2ObjectSpawnPosition.x = Random.Range(-8f,-14f);
 				PhotonNetwork.Instantiate (player2Obj5.name, player2ObjectSpawnPosition, Quaternion.identity, 0, null);
-				GameUI.score -= unit1Cost;
+				GameUI.score -= unit5Cost;
 			}
 		}
 	}
