38b7e840ecbae779de4afe1f4d96aecf4acf278d
Scott Stephan <ss2111@msstate.edu>
2015-05-05 20:23:16 -0500

    Waiting Screen and Menu Updates
    Added the waiting screen and resetting the score. Also added the menu
    updates. The waiting screen needs an actual sprite.

--- Castle-Rush/Assets/Menu.cs
+++ Castle-Rush/Assets/Menu.cs
@@ -3,14 +3,16 @@ using System.Collections;
 
 public class Menu : MonoBehaviour {
 	public Texture2D texture = null;
-	public Texture2D texture2 = null;
-	public Texture2D texture3 = null;
-	public Texture2D texture4 = null;
-	public Texture2D texture5 = null;
-	public Texture2D texture6 = null;
-	public Texture2D texture7 = null;
-	public Texture2D texture8 = null;
-	public Texture2D texture9 = null;
+	public Texture2D Logo1 = null;
+	public Texture2D texture_helpButton = null;
+	public Texture2D texture_QuitGame = null;
+	public Texture2D texture_BackButton = null;
+	public Texture2D texture_StartGame = null;
+	public Texture2D texture_LobbyButton = null;
+	public Texture2D texture_RefreshButton = null;
+	public Texture2D texture_ServerList = null;
+	public Texture2D texture_HelpMenu = null;
+	public Texture2D texture_smallBackButton = null;
 
 	private string CurMenu;
 	public string Name;
@@ -44,56 +46,68 @@ public class Menu : MonoBehaviour {
 		if(CurMenu == "Lobby"){
 			Lobby();
 		}
+		if (CurMenu == "HowTo") {
+			HowTo ();
+		}
 
 	}
 
 	private void Main(){
+		//Game Logo
+		GUI.Label ( new Rect(Screen.width/(Screen.height/50), Screen.height/Screen.width, Logo1.width, Logo1.height), Logo1);
+
 		//Host Game
 		if (GUI.Button (new Rect(Screen.width/(Screen.height/90), Screen.height/(Screen.width/400), texture.width, texture.height), texture)) {
 			ToMenu ("Host");
 		}
 		//Quit Game
-		if (GUI.Button (new Rect(Screen.width/(Screen.height/241)-103, Screen.height/(Screen.width/600)+63, texture3.width, texture3.height), texture3)) {
+		if (GUI.Button (new Rect(Screen.width/(Screen.height/241)-103, Screen.height/(Screen.width/600)+63, texture_QuitGame.width, texture_QuitGame.height), texture_QuitGame)) {
 			Application.Quit();
 		}
 		//How to play
-		if (GUI.Button (new Rect (Screen.width/(Screen.height/90), Screen.height/(Screen.width/600)+63, texture2.width, texture2.height), texture2)) {
-			
+		if (GUI.Button (new Rect (Screen.width/(Screen.height/90), Screen.height/(Screen.width/600)+63, texture_helpButton.width, texture_helpButton.height), texture_helpButton)) {
+			ToMenu ("HowTo");
 		}
 		//Enter Lobby Button
-		if (GUI.Button (new Rect (Screen.width/(Screen.height/241)-103, Screen.height/(Screen.width/400), texture4.width, texture4.height), texture7)) {
+		if (GUI.Button (new Rect (Screen.width/(Screen.height/241)-103, Screen.height/(Screen.width/400), texture_LobbyButton.width, texture_LobbyButton.height), texture_LobbyButton)) {
 			ToMenu ("Lobby");
 		}
 
 	}
 
 	private void Host(){
+		//Game Logo
+		GUI.Label ( new Rect(Screen.width/(Screen.height/50), Screen.height/Screen.width, Logo1.width, Logo1.height), Logo1);
+
 		MatchName = GUI.TextField(new Rect(Screen.width/(Screen.height/100), Screen.height/(Screen.width/400), 644, texture.height), MatchName, 48, host_style);
 		//Start Game Button
-		if (GUI.Button (new Rect(Screen.width/(Screen.height/90), Screen.height/(Screen.width/600)+63, texture.width, texture.height), texture6)) {
+		if (GUI.Button (new Rect(Screen.width/(Screen.height/90), Screen.height/(Screen.width/600)+63, texture.width, texture.height), texture_StartGame)) {
 			PlayerPrefs.SetString("HostName", MatchName);
 			NetworkManager.Instance.StartServer(MatchName, Players);
 			//ToMenu ("Lobby");
 			PhotonNetwork.LoadLevel("FullGame");
 		}
 		//Back Button
-		if (GUI.Button (new Rect(Screen.width/(Screen.height/241)-103, Screen.height/(Screen.width/600)+63, texture.width, texture.height), texture5)) {
+		if (GUI.Button (new Rect(Screen.width/(Screen.height/241)-103, Screen.height/(Screen.width/600)+63, texture.width, texture.height), texture_BackButton)) {
 			ToMenu ("Main");
 		}
 	}
 
 	private void Lobby(){
+		//Game Logo
+		GUI.Label ( new Rect(Screen.width/(Screen.height/50), Screen.height/Screen.width, Logo1.width, Logo1.height), Logo1);
+
 		//Refresh Button
-		if (GUI.Button (new Rect(Screen.width/(Screen.height/90), Screen.height/(Screen.width/400), texture.width, texture.height), texture8)) {
+		if (GUI.Button (new Rect(Screen.width/(Screen.height/90), Screen.height/(Screen.width/400), texture.width, texture.height), texture_RefreshButton)) {
 			MasterServer.RequestHostList("CastleRush");
 		}
 
 		//Back Button
-		if (GUI.Button (new Rect(Screen.width/(Screen.height/241)-103, Screen.height/(Screen.width/400), texture.width, texture.height), texture5)) {
+		if (GUI.Button (new Rect(Screen.width/(Screen.height/241)-103, Screen.height/(Screen.width/400), texture.width, texture.height), texture_BackButton)) {
 			ToMenu ("Main");
 		}
-		GUI.Label(new Rect (Screen.width/(Screen.height/100), Screen.height/(Screen.width/600)+6, texture9.width, texture9.height), texture9);
-		GUILayout.BeginArea (new Rect (Screen.width/(Screen.height/90)+6, Screen.height/(Screen.width/600)+120, texture9.width, 200), "","box");
+		GUI.Label(new Rect (Screen.width/(Screen.height/100), Screen.height/(Screen.width/600)+6, texture_ServerList.width, texture_ServerList.height), texture_ServerList);
+		GUILayout.BeginArea (new Rect (Screen.width/(Screen.height/90)+6, Screen.height/(Screen.width/600)+120, texture_ServerList.width, 200), "","box");
 		foreach (HostData hd in MasterServer.PollHostList()) {
 			GUILayout.BeginHorizontal();
 			GUILayout.Label(hd.gameName);
@@ -105,6 +119,16 @@ public class Menu : MonoBehaviour {
 			GUILayout.EndHorizontal();
 		}
 		GUILayout.EndArea ();
+
+	}
+	private void HowTo(){
+		//Help Menu
+		GUI.Label ( new Rect(Screen.width/(Screen.height), Screen.height/Screen.width, texture_HelpMenu.width, texture_HelpMenu.height), texture_HelpMenu);
+
+		//Back Button
+		if (GUI.Button (new Rect(Screen.width/(Screen.height), Screen.height/(Screen.width), texture_smallBackButton.width, texture_smallBackButton.height), texture_smallBackButton)) {
+			ToMenu ("Main");
+		}
 
 	}
 
---
+++ Castle-Rush/Assets/StartGame.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent( typeof( PhotonView ) )]
+public class StartGame : Photon.MonoBehaviour {
+
+	public GameObject waitingScreen;
+	public int PlayerCount;
+	public AudioSource GemMatch;
+	public bool gameStart = false;
+	// Use this for initialization
+	void Start () {
+		GemMatch.mute = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (PhotonNetwork.player.ID != PhotonNetwork.masterClient.ID && !gameStart) {
+			photonView.RPC ("MoveBlocker", PhotonTargets.All);
+		}
+		if (!gameStart) {
+			GameUI.score = 0;
+		}
+	}
+
+	[RPC]
+	public void MoveBlocker(){
+		gameStart = true;
+		GemMatch.mute = false;
+		GameUI.score = 0;
+		waitingScreen.SetActive (false);
+	}
+	/*
+	[RPC]
+	public void Connected(){
+		PlayerCount += 1;
+		print( "OnPhotonPlayerConnected()" ); // not seen if you're the player connecting
+	}
+	*/
+}
